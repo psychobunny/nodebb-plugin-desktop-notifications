5b02240a78ae7b2a3d42a9865b604026ce773f4b
psychobunny <psycho.bunny@hotmail.com>
2013-12-29 20:43:48 -0500

    woops, bug introduced in 0.0.3

--- public/lib/main.js
+++ public/lib/main.js
@@ -2,32 +2,36 @@
 	"use strict";
 
 	function requestPermission() {
-		(new Notify('NodeBB')).requestPermission();
+		require(['notify'], function(Notify) {
+			(new Notify('NodeBB')).requestPermission();
+		});
 	}
 
 	jQuery('document').ready(function() {
-		require(['notify'], function(Notify) {
-			var logo = $('.forum-logo').attr('src');
+		var logo = $('.forum-logo').attr('src');
 
+		requestPermission();
+
+		jQuery('#notif_dropdown').on('click', function() {
 			requestPermission();
+		});
 
-			jQuery('#notif_dropdown').on('click', function() {
-				requestPermission();
-			});
-			
-			socket.on('event:new_notification', function(data) {
-				if (data.text) {
-					var notification = new Notify('NodeBB', {
-						body: data.text.replace(/<strong>/g, '').replace(/<\/strong>/g, ''),
-						icon: logo,
-						notifyClick: function() {
-							ajaxify.go(data.path.substring(1));
-						}
-					});
+		jQuery('*').on('click', function() {
+			requestPermission();
+		});
+		
+		socket.on('event:new_notification', function(data) {
+			if (data.text) {
+				var notification = new Notify('NodeBB', {
+					body: data.text.replace(/<strong>/g, '').replace(/<\/strong>/g, ''),
+					icon: logo,
+					notifyClick: function() {
+						ajaxify.go(data.path.substring(1));
+					}
+				});
 
-					notification.show();
-				}
-			});
+				notification.show();
+			}
 		});
 	});
 }());
