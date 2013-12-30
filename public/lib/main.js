1fbc7f437decdb6a80fe2b59686db607a41ab599
psychobunny <psycho.bunny@hotmail.com>
2013-12-29 20:54:13 -0500

    fixed another bug, I really shouldn't be coding this late

--- public/lib/main.js
+++ public/lib/main.js
@@ -22,12 +22,14 @@
 		
 		socket.on('event:new_notification', function(data) {
 			if (data.text) {
-				var notification = new Notify('NodeBB', {
-					body: data.text.replace(/<strong>/g, '').replace(/<\/strong>/g, ''),
-					icon: logo,
-					notifyClick: function() {
-						ajaxify.go(data.path.substring(1));
-					}
+				require(['notify'], function(Notify) {
+					var notification = new Notify('NodeBB', {
+						body: data.text.replace(/<strong>/g, '').replace(/<\/strong>/g, ''),
+						icon: logo,
+						notifyClick: function() {
+							ajaxify.go(data.path.substring(1));
+						}
+					});
 				});
 
 				notification.show();
