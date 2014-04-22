1304ae85767917d9cd646d0ee5ac273d94f19767
Barış Soner Uşaklı <barisusakli@gmail.com>
2014-04-22 01:49:47 -0400

    translate before creating

--- public/lib/main.js
+++ public/lib/main.js
@@ -21,19 +21,22 @@
 		});
 		
 		socket.on('event:new_notification', function(data) {
-			if (data.text) {
+			if (!data || !data.text) {
+				return;
+			}
+
+			translator.translate(data.text, function(translated) {
 				require(['notify'], function(Notify) {
 					var notification = new Notify('NodeBB', {
-						body: data.text.replace(/<strong>/g, '').replace(/<\/strong>/g, ''),
+						body: translated.replace(/<strong>/g, '').replace(/<\/strong>/g, ''),
 						icon: logo,
 						notifyClick: function() {
 							ajaxify.go(data.path.substring(1));
 						}
 					});
-
 					notification.show();
 				});
-			}
+			});
 		});
 	});
 }());
