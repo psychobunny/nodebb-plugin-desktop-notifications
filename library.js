7befd5710c3a78e9d861733f0389c0fc512778a7
Barış Soner Uşaklı <barisusakli@gmail.com>
2018-11-30 19:41:36 -0500

    fix require, closes #33

--- library.js
+++ library.js
@@ -1,19 +1,15 @@
-(function(module) {
-	"use strict";
+"use strict";
 
-	var notifications = {},
-		app;
+var notifications = module.exports;
+var app;
 
-	notifications.pushed = function(params, callback) {
-		var notification = params.notification,
-			uids = params.uids;
+notifications.pushed = function(params, callback) {
+	var notification = params.notification;
+	var uids = params.uids;
 
-		var socket = module.parent.require('./socket.io');
+	var socket = require.main.require('./src/socket.io');
 
-		uids.forEach(function(uid) {
-			socket.in('uid_' + uid).emit('event:plugin:desktop_notifications', notification);
-		});
-	};
-
-	module.exports = notifications;
-}(module));
+	uids.forEach(function(uid) {
+		socket.in('uid_' + uid).emit('event:plugin:desktop_notifications', notification);
+	});
+};
